aa90162b503c6f289650c33c6202d9b9ae5fdf3d
Nikita Davydov <nikita.davidov02@yandex.ru>
2024-01-25 14:37:29 +0300

    Initial commit

---
+++ Polymer brush/Matrix.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymer_brush
+{
+    static class Matrix
+    {
+        public static double determinantGauss(int n, double[,] M, int currentColumn, bool changeSign)
+        {
+            double[,] A = CopyMatrix(n, M);
+            //Calculate determinant if there are no rows
+            if (currentColumn == n)
+            {
+                double det = 1;
+                for (int i = 0; i < n; i++)
+                    det *= A[i,i];
+                if (changeSign)
+                    det *= -1;
+                return det;
+            }
+            //Find first non-zero element in a current column
+            int nonZeroRow = -1;
+            for (int i = currentColumn; i < n; i++)
+            {
+                if (A[i,currentColumn] != 0)
+                {
+                    nonZeroRow = i;
+                    break;
+                }
+            }
+            if (nonZeroRow == -1)
+                return 0;
+            //Change strings, change sign
+            if (nonZeroRow != currentColumn)
+            {
+                double nonZeroRowCopy = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    nonZeroRowCopy = A[nonZeroRow,i];
+                    A[nonZeroRow,i] = A[currentColumn,i];
+                    A[currentColumn,i] = nonZeroRowCopy;
+                }
+                //
+                if (changeSign)
+                    changeSign = false;
+                else
+                    changeSign = true;
+            }
+            //Substract first column from other columns
+            for (int i = currentColumn + 1; i < n; i++)
+            {
+                if (A[i,currentColumn] != 0)
+                {
+                    //Substract rows
+                    double coeffitient = A[i,currentColumn] / A[currentColumn,currentColumn];
+                    for (int j = 0; j < n; j++)
+                    {
+                        A[i,j] -= A[currentColumn,j] * coeffitient;
+                    }
+                }
+            }
+            //Recursion
+            currentColumn++;
+            return determinantGauss(n, A, currentColumn, changeSign);
+        }
+        public static double[,] CopyMatrix(int n, double[,] A)
+        {
+            double[,] copy = new double[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    copy[i,j] = A[i,j];
+            }
+            return copy;
+        }
+
+        //Функции матрицчной алгебры
+        public static double[,] unitMatrix(int n)
+        {
+            double[,] matrix = new double[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i,j] = 0;
+                    if (i == j)
+                        matrix[i,j] = 1;
+                }
+            }
+            return matrix;
+        }
+        public static double[,] multiplyMatrixes(int ai, int bj, int m, double[,] A, double[,] B)
+        {
+            double[,] C = new double[ai, bj];
+            for (int i = 0; i < ai; i++)
+            {
+                for (int j = 0; j < bj; j++)
+                {
+                    double c = 0;
+                    for (int k = 0; k < m; k++)
+                        c += A[i,k] * B[k,j];
+                    C[i,j] = c;
+                }
+            }
+            return C;
+        }
+        public static double determinant(int n, double[,] A, int[] expeledColumns, int[] expeledRows, int expeledRowsAndColumns)
+        {
+            if (expeledColumns == null)
+            {
+                expeledColumns = new int[n];
+                for (int i = 0; i < n; i++)
+                    expeledColumns[i] = -1;
+            }
+            if (expeledRows == null)
+            {
+                expeledRows = new int[n];
+                for (int i = 0; i < n; i++)
+                    expeledRows[i] = -1;
+            }
+            //for (int i = 0; i < n; i++)
+            //    std::cout << expeledColumns[i] << "\n";
+            //expeledRowsAndColumns++;
+
+            if (expeledRowsAndColumns == n - 1)
+            {
+                int leftColumn = -1;
+                int leftRow = -1;
+                for (int i = 0; i < n; i++)
+                    if (arrayContains(expeledColumns, i, n) == -1)
+                        leftColumn = i;
+                for (int i = 0; i < n; i++)
+                    if (arrayContains(expeledRows, i, n) == -1)
+                        leftRow = i;
+                return A[leftRow,leftColumn];
+            }
+
+            //Find not expeled columns for next determinant
+            int[] notExpeledColumns = new int[n - expeledRowsAndColumns];
+            int j = 0;
+            for (int i = 0; i < n; i++)
+                if (arrayContains(expeledColumns, i, n) == -1)
+                {
+                    notExpeledColumns[j] = i;
+                    j++;
+                }
+            int[] notExpeledRows = new int[n - expeledRowsAndColumns];
+            j = 0;
+            for (int i = 0; i < n; i++)
+                if (arrayContains(expeledRows, i, n) == -1)
+                {
+                    notExpeledRows[j] = i;
+                    j++;
+                }
+
+            double output = 0;
+
+
+            for (int i = 0; i < n - expeledRowsAndColumns; i++)
+            {
+                int indexOfExpelingRow = notExpeledRows[0];
+                int indexOfExpelingColumn = notExpeledColumns[i];
+                expeledColumns[expeledRowsAndColumns] = indexOfExpelingColumn;
+                expeledRows[expeledRowsAndColumns] = indexOfExpelingRow;
+
+                expeledRowsAndColumns++;
+                double dt = determinant(n, A, expeledColumns, expeledRows, expeledRowsAndColumns);
+                expeledRowsAndColumns--;
+
+                if (i % 2 == 0)
+                    output += A[indexOfExpelingRow,indexOfExpelingColumn] * dt;
+                else
+                    output -= A[indexOfExpelingRow,indexOfExpelingColumn] * dt;
+
+            }
+            expeledColumns[expeledRowsAndColumns] = -1;
+            expeledRows[expeledRowsAndColumns] = -1;
+            return output;
+        }
+        public static int arrayContains(int[] a, int v, int aSize)
+        {
+            //int n= sizeof(a) / sizeof(int);
+            for (int i = 0; i < aSize; i++)
+            {
+                if (a[i] == v)
+                    return i;
+            }
+            return -1;
+        }
+        public static double[,]reverseMatrix(int n, double[,] M)
+        {
+            double[,] reverseMatrix = zeroMatrix(n);
+            double[,] A = CopyMatrix(n, M);
+            //double detA = determinant(n, A, NULL, NULL, 0);
+            double detA = determinantGauss(n, A, 0, false);
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    /* int* expeledColumn = new int[n];
+                     int* expeledRow = new int[n];
+                     for (int k = 0; k < n; k++)
+                         expeledColumn[i] = -1;
+                     for (int k = 0; k < n; k++)
+                         expeledRow[i] = -1;
+                     expeledColumn[0] = j;
+                     expeledRow[0] = i;
+
+                     double minor = determinant(n, A, expeledColumn, expeledRow, 1);*/
+                    double[,] minorMatrix = new double[n-1, n - 1];
+                    //for (int k = 0; k < n - 1; k++)
+                    //    minorMatrix[k] = new double[n - 1];
+                    for (int a = 0; a < n; a++)
+                        for (int b = 0; b < n; b++)
+                        {
+                            if (a < i)
+                            {
+                                if (b < j)
+                                    minorMatrix[a,b] = A[a,b];
+                                if (b > j)
+                                    minorMatrix[a,b - 1] = A[a,b];
+                            }
+                            if (a > i)
+                            {
+                                if (b < j)
+                                    minorMatrix[a - 1,b] = A[a,b];
+                                if (b > j)
+                                    minorMatrix[a - 1,b - 1] = A[a,b];
+                            }
+                        }
+                    ////////////////////////////////////////////////////////////////
+                    //double minor = determinantGauss(n, A, expeledColumn, expeledRow, 1);
+                    double minor = determinantGauss(n - 1, minorMatrix, 0, false);
+                    //double v = (pow(-1, (i + j) % 2)) * minor / detA;
+                    double v = minor / detA;
+                    if ((i + j) % 2 == 1)
+                        v = -v;
+                    reverseMatrix[j,i] = v;
+                }
+            return reverseMatrix;
+        }
+        public static double[,] zeroMatrix(int n)
+        {
+            double[,] matrix = new double[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i,j] = 0;
+                }
+            }
+            return matrix;
+        }
+        public static double[] multiplyMatrixAndVector(int ai, int bj, double[,] A, double[] B)
+        {
+            double[] C = new double[ai];
+            for (int i = 0; i < ai; i++)
+            {
+                C[i] = 0;
+                for (int k = 0; k < bj; k++)
+                    C[i] += A[i,k] * B[k];
+            }
+            return C;
+        }
+    }
+}
---
+++ Polymer brush/Program.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Polymer_brush
+{
+	class Program
+	{
+		static double R, BA, y_min, y_max, yacc, aA, rNA, rNB, sigma, Lamb_Pol;
+		static double rN, nu, coe, pi, osmbulk, y_try, y_edge, y_cur, P_AB;
+		static double[] lambda, Fibulk, Lagrbulk, Nal;
+		static double[,] chi;
+		static double[] Xbrush, fipolimer;
+		static double point_y;
+		static int i, j;
+		static StreamWriter sw;
+
+		static void Main(string[] args)
+		{
+			/*double[] guess = new double[2];
+			double[] X;
+			double norm;
+			guess[0] = 2;
+			guess[1] = 2;
+			DNEQNF(TestSystem, 0.0001, 2, 100, guess, out X, out norm);
+			Console.WriteLine("X[0] = " + X[0]);
+			Console.WriteLine("X[1] = " + X[1]);
+			Console.ReadLine();*/
+
+			double Fmix = 0;
+			double u_sol = 0;
+			double u_pol = 0;
+			double u_bio=0;
+			sw = new StreamWriter("profile.txt");
+			sw.WriteLine("y_cur    solvent    bio    polymer    osm_pressure");
+
+			Enter();
+			y_try = y_min;
+			y_edge = rtbis3(y_min, y_max, yacc);
+			y_cur = y_min;
+
+			//!Calculate polymer concentration at the start position(A / B boundary)
+			P_AB = fiav(y_cur, y_edge); // !   Polymer concentration at A/ B boundary(highest)  must work before finding profile to set Lagr multipliers in common block
+           
+			
+			while (y_cur < y_edge)
+            {
+				y_cur += aA / R;
+				//!write(*, *) 'Phi poly subroutine is called'
+				Xbrush = new double[3];
+				FI_POLI(out Xbrush, y_cur); //  !calculates concentration profile in the brush after the solution is found
+
+				
+				fipolimer[1] = Xbrush[0];// ! local compsition of the brush at point y_cur(1)-solvent(2) - biocomponent(3) - polymer
+				fipolimer[2] = Xbrush[1];
+				fipolimer[0] = 1.0 - fipolimer[1] - fipolimer[2];
+
+				Console.Write("Fmix="+Fmix);
+				Console.Write("u_sol=" +u_sol);
+				Console.Write("u_pol=" + u_pol); 
+				Console.Write("u_bio=" + u_bio);
+				Console.Write("/n");
+
+				sw.WriteLine(y_cur.ToString()+"    "+fipolimer[0] + "    " + fipolimer[1] + "    " + fipolimer[2] + "    " + (Osmmix(3, fipolimer) - osmbulk));
+			}
+
+			Console.WriteLine("Beta: " + y_edge);
+			Console.WriteLine("Calculation is done!");
+
+
+			sw.Close();
+		}
+		static void Enter()
+        {
+			pi = 3.1415926;
+			coe = (3.0 / 8.0) * pi * pi;
+			aA = 6.8 * Math.Pow(10, -9);
+			rN = 80.0; //total number of polymer segments per chain
+			rNA = 60.0; //number of segments in A - subchain
+			rNB = rN - rNA;
+			nu = 2.0; //spherical micelle
+			sigma = (7.0d - 09) * (7.0d - 09) / 0.12;
+			sigma = (7.0d - 09) * (7.0d - 09) / 0.6;
+			R = rNB * (nu + 1) * aA * aA * aA / sigma; // core radius((nu+1.0)*rNB / SIGMA)**2    LAGRANGE_STR1 = BA * (y_try - 1.0) * *2 * ((nu + 1.0) * rNB / SIGMA) * *2 * aA * *3  CHECK THIS
+													   //write(*, *) R,sigma
+													   //
+			BA = coe / ((rNA * aA) * (rNA * aA));
+
+			y_min = 1.0 + 0.1 * aA / R;
+			y_max = 1.00001 * (1.0 + 1.0 * aA * rNA / R);                 //y_max = 1.0d0 * (1.0d0 + 2.0 * aA * rNA / R)
+			yacc = Math.Pow(10, -8);
+
+			//! sigma min for all morphologies:
+			//!sigma_MIN = 10.0 * (nu + 1.1) * aA * *2
+			//!sigma_MAX = 3.0d0 * aA * *2 * (3.1415926 * 4.0 * rNB * *2 / 3.0d0) **(1.0 / 3.0)
+			//				 !write(*, *) BA,R,rNB* aA, sigma,10.0 * (nu + 1.1) * aA * *2,y_max,BA * (R * (y_max - 1.0)) * *2,
+			//!stop
+
+			Nal = new double[3];
+			Nal[0] = 1.0;// ! solvent
+			Nal[1] = 3.0;// ! bioadditive
+			Nal[2] = rNA;// polymer
+
+			chi = new double[3, 3];
+
+			chi[0, 1] = 1.0;//! solv - bio
+			chi[0, 2] = 0.0;//! solv - polym
+			chi[1, 2] = -0.8;//d0! bio - polym
+
+			for (int i = 0; i < 3; i++)
+			{
+				chi[i, i] = 0;
+				for (int j = i + 1; j < 3; j++)
+					chi[j, i] = chi[i, j];
+			}
+
+			//!give bulk composition and calculate Lagr.multipliers and osm pressure in the bulk:
+			Fibulk = new double[3];
+			Fibulk[0] = 0.98;
+			Fibulk[1] = 1.0 - Fibulk[0];
+			Fibulk[2] = 0.0;
+
+			lambda = new double[3];
+			fipolimer = new double[3];
+			Lagrmix(2, Fibulk, out Lagrbulk);
+			lambda[1] = Lagrbulk[1];//this is for biocomponent
+			osmbulk = Osmmix(2, Fibulk);// !  this is for solvent
+
+			lambda[0] = Lagrbulk[0]; //!  this should be identiacally zero
+
+			//! now calculate  mixing part of Lagrange multiplier at the edge of the brush, where  FiA = 0:
+
+			Fibulk[2] = 0.0;
+
+			Lagrmix_PolA(3, Fibulk, out Lagrbulk);// ! NOTE  that  Lagr multipliers are spoiled in  Lagrbulk   but stored in common
+
+			lambda[2] = Lagrbulk[2];
+
+		}
+		static void Lagrmix(int numberOfComponents, double[] X, out double[] DfmixDfi)
+		{
+			DfmixDfi = new double[3];
+			for (int i = 0; i < numberOfComponents; i++)
+			{
+				double sum = 0;
+				for (int j = 0; j < numberOfComponents; j++)
+					sum += X[j] * (chi[i, j] - chi[0, j]);
+				DfmixDfi[i] = (Math.Log(X[i]) + 1.0) / Nal[i] - (Math.Log(X[0]) + 1.0) / Nal[0] + sum;// ! dummy for solvent identically 0
+																									  //AlternativeDfmixDfi(i) = CalculateExchangeChemialPotentialOfComponent(3, X, i)
+																									  //DfmixDfi(i) = AlternativeDfmixDfi(i)
+			}
+		}
+		static void Lagrmix_PolA(int numberOfComponents, double[] X, out double[] DfmixDfi)
+		{
+			DfmixDfi = new double[3];
+			for (int i = 0; i < numberOfComponents; i++)
+			{
+				if (X[i] < 0)
+				{
+					for (int n = 0; n < numberOfComponents; n++)
+						DfmixDfi[n] = 1050;
+					return;
+				}
+
+				double sum = 0;
+				for (int j = 0; j < numberOfComponents; j++)
+					sum += X[j] * (chi[i, j] - chi[0, j]);
+				if (i == numberOfComponents - 1)
+					DfmixDfi[i] = -(Math.Log(X[0]) + 1.0) / Nal[0] + sum;// ! dummy for solvent identically 0
+				else
+					DfmixDfi[i] = (Math.Log(X[i]) + 1.0) / Nal[i] - (Math.Log(X[0]) + 1.0) / Nal[0] + sum;// ! dummy for solvent  identically 0
+			}
+		}
+		static double Osmmix(int numberOfComponents, double[] X)
+		{
+			double sum = 0;
+			double sum1 = 0;
+			for (int i = 0; i < numberOfComponents; i++)
+			{
+				sum1 = sum1 + X[i] * (1.0 - 1.0 / Nal[i]);
+				for (int j = 0; j < numberOfComponents; j++)
+					sum += X[i] * X[j] * (chi[0, j] - chi[i, j] / 2.0);
+			}
+			return Math.Log(X[0]) + sum1 + sum;
+		}
+		static double normal(double y)
+		{
+			double nu = 2.0;
+			double aINT = 1.0;
+			double bINT = y_try;
+			double s = 1;
+			qtrap(aINT, bINT, out s);
+			return s - rNA / (rNB * (nu + 1.0));
+
+		}
+		static void qtrap(double aINT, double bINT, out double s)
+        {
+			double EPS = 0.1;
+			int JMAX = 8;
+			double old_s = -1 * Math.Pow(10, -30);
+			s = 0;
+			for(int n = 0; n < JMAX;n++)
+            {
+				trapzd(aINT, bINT, out s, n);
+                if (n > 5)
+					if (Math.Abs(s - old_s) < EPS * Math.Abs(old_s) || (s == 0 && old_s == 0))
+						return;
+				old_s = s;
+            }
+			Console.WriteLine("Too many steps in q trap");
+			throw new Exception();
+
+
+		}
+		static void trapzd(double aINT,double bINT, out double s, int n)
+        {
+			s = 0;
+			if (n == 1)
+				s = 0.5 * (bINT - aINT) * (fiav(aINT, bINT) + fiav(bINT, bINT));
+            else
+            {
+				int it = (int)Math.Pow(2, n - 2);
+				double tnm = it;
+				double del = (bINT - aINT) / tnm;
+				double x = aINT + 0.5 * del;
+				double sum = 0;
+				for(int counter = 0; counter < it; counter++)
+                {
+					sum += fiav(x, bINT);
+					x += del;
+                }
+				s = 0.5 * (s + (bINT - aINT) * sum / tnm);
+			}
+		}
+		static double fiav(double y_cur, double bINT)
+        {
+			int L = 2;
+			double nu = 2;
+			double y_try = bINT;
+			Lamb_Pol = lambda[2] + BA * (R * (y_try - 1)) * (R * (y_try - 1));
+			FI_POLI(out Xbrush, y_cur);
+			double fay = Xbrush[1];
+			return fay * y_cur * y_cur;
+
+		}
+		static double rtbis3(double y_min, double y_max, double yacc)
+		{
+			double output = 0;
+			int j_max = 100; //!Maximum allowed number of bisections.
+							 //!Using bisection, find the root of a function func known to lie between x1 and x2.The
+							 //!root, returned as rtbis, will be refined until its accuracy is ï¿½xacc.
+			double dx, f, fmid, xmid;
+			//!calculating function value at y_max
+			fmid = normal(y_max);
+			f = normal(y_min);// !R2(beta2, lambda, itog2)
+
+			if (f * fmid > 0)
+			{
+				Console.WriteLine("Bisection error");
+				throw new Exception();
+				return 0;
+			}
+			if (f < 0)
+			{
+				// then!Orient the search so that f> 0 lies at x + dx.
+				output = y_min;
+				dx = y_max - y_min;
+			}
+			else
+			{
+				output = y_max;
+				dx = y_min - y_max;
+
+			}
+			for (int j = 0; j < j_max; j++)
+			{
+				dx = dx / 2;
+				xmid = output + dx;
+				fmid = normal(xmid);// !call R3(beta3, lambda, itog3)
+				if (fmid < 0)
+					output = xmid;
+				if (Math.Abs(dx) < yacc || fmid == 0)
+					return output;
+			}
+			Console.WriteLine("Too many iterations");
+			throw new Exception();
+		}
+		static void FI_POLI(out double[] XBrush, double y_cur)
+        {
+			//!Solving(localy) system of non - linear equations
+
+			int L = 2;
+			double ERREL = Math.Pow(10, -4);
+			point_y = y_cur;
+			int ITMAX = 600;
+			double[] XBrushGUESS = new double[L];
+			XBrush = new double[L];
+			XBrushGUESS[0] = Math.Pow(10, -8);//this is the fraction of biocomponent in the brush
+			XBrushGUESS[1]=0.97;//this is the fraction of polymer in the brush
+			double FNORM;
+
+			DNEQNF(EU_LA, ERREL, L, ITMAX, XBrushGUESS, out XBrush, out FNORM);
+
+		}
+		delegate void NonlinearSystem(double[] XBrush, out double[] F, int L);
+		static void DNEQNF(NonlinearSystem Func, double ERREL, int L, int ITMAX, double[] XGuess, out double[] X, out double FNORM)
+		{
+			double[] F = new double[L];
+			X = new double[XGuess.Length];
+			for (int i = 0; i < XGuess.Length; i++)
+				X[i] = XGuess[i];
+			FNORM = 0;
+			Func(X, out F, L);
+			for (int i = 0; i < L; i++)
+				FNORM += F[i]*F[i];
+
+			double[,] J = new double[L, XGuess.Length];
+            while (FNORM >= ERREL)
+            {
+				//Calculate Jacobian
+				for(int i=0;i<L;i++)
+					for(int j=0;j< X.Length; j++)
+                    {
+						Func(X, out F, L);
+						double f_init = F[i];
+						double old_x = X[j];
+						double dx = old_x * 0.01;
+						X[j] += dx;
+						Func(X, out F, L);
+						double f_df = F[i];
+						J[i, j] = (f_df - f_init) / dx;
+						X[j] = old_x;
+						Func(X, out F, L);
+					}
+				double det = Matrix.determinantGauss(L, J, 0, false);
+				double[,] reverse = Matrix.reverseMatrix(L, J);
+				double[] rightParts = new double[L];
+				for(int i = 0; i < L; i++)
+                {
+					rightParts[i] = -F[i];
+					for (int j = 0; j < X.Length; j++)
+						rightParts[i] += J[i, j] * X[j];
+                }
+				X = Matrix.multiplyMatrixAndVector(L, L, reverse, rightParts);
+
+				FNORM = 0;
+				for (int i = 0; i < L; i++)
+					FNORM += F[i] * F[i];
+			}
+			
+        }
+		static void EU_LA(double[] XBrush, out double[] F, int L)
+        {
+			double nu = 2;
+			double y_cur = point_y;
+			fipolimer[1] = Xbrush[0];
+			fipolimer[2] = Xbrush[1];
+			fipolimer[0] = 1 - fipolimer[1] - fipolimer[2];
+			double[] DfmixDfi;
+			//!Calculate values that in ideal case must be equal to Lagrangian multipliers based on current concentrations
+			Lagrmix_PolA(3, fipolimer, out DfmixDfi);
+			F = new double[L];
+
+			F[0] = (DfmixDfi[1] - lambda[1]) * (DfmixDfi[1] - lambda[1]);// !bio contaminant error
+			F[1] = Math.Pow((DfmixDfi[2] + BA * (R * (y_cur - 1.0)) * (R * (y_cur - 1.0)) - Lamb_Pol),2);//!polymer error
+
+			Console.WriteLine("EU_LA " + F + "    " + Xbrush[0] + "    " + Xbrush[1]);
+
+		}
+		static void TestSystem(double[] X, out double[] F, int L)
+        {
+			F = new double[L];
+			F[0] = X[0] * X[0] + X[1] * X[1] - 1;
+			F[1] = X[1] - X[0] * X[0];
+		}
+	}
+}
