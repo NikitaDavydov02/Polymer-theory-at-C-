6360bb106950b3711089fc9e2afb6171c8e688dd
Nikita Davydov <nikita.davidov02@yandex.ru>
2024-03-21 23:52:22 +0300

    Input files are added but not serialization done yet

---
+++ Polymer brush/Components/Component.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymer_brush.Components
+{
+    public class Component
+    {
+        public ComponentType Type;
+        public string Name;
+        public int Size;
+
+    }
+    public class PolymerComponent : Component
+    {
+        public double KunLength;
+        public double AreaPerChain;
+        public double NtotalSegments;
+        public double NgroupTypes;
+        public List<double> groupsFractions;
+    }
+    public enum ComponentType
+    {
+        Polymer,
+        Solvent,
+        Additive,
+    }
+}
---
+++ Polymer brush/Input.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polymer_brush.Components;
+
+namespace Polymer_brush
+{
+    [Serializable]
+    class Input
+    {
+        public List<Component> Components;
+        public Dictionary<Component, double> VolumeFractionsInTheBulk;
+        public double[,] Chi;
+
+        
+    }
+}
